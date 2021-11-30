ad5e89f76655fd18803a69ea6901409a5a5c59b0
ali-alinezhad <ali-alinezhad@gmail.com>
2021-11-30 21:57:49 +0100

    lillydoo2: impl insert with upload and add templates

---
+++ src/AppBundle/Controller/AddressBookController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\AddressBook;
+use AppBundle\Form\AddressBookType;
+use AppBundle\Service\FileUploader;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AddressBookController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $addressBook = new AddressBook();
+        $form        = $this->createForm(AddressBookType::class, $addressBook);
+
+        return $this->render('address_book/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/insert/contact", name="insert.contact")
+     */
+    public function insert(Request $request, FileUploader $fileUploader)
+    {
+        $address = new AddressBook();
+        $form = $this->createForm(AddressBookType::class, $address);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $picture */
+            $picture = $form->get('picture')->getData();
+
+            if ($picture) {
+                $fileName = $fileUploader->upload($picture);
+                $address->setPicture($fileName);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($address);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Contact successfully inserted!');
+            return $this->redirectToRoute('homepage');
+        }
+
+        $this->addFlash('errors', 'Something went wrong!');
+        return $this->redirectToRoute('homepage');
+    }
+}
